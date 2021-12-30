7fe0da8c7ed5f1445e7319a69c8d29da522242d1
Olayanju Ajibola <ajibolaolayanju@gmail.com>
2021-12-30 14:59:51 +0100

    flutter deployed now

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,16 +1,3 @@
-/* const path = require('path');
-
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
-    connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-    },
-    useNullAsDefault: true,
-  },
-});
- */
- 
 const parse = require('pg-connection-string').parse;
 const config = parse(process.env.DATABASE_URL);
 module.exports = ({ env }) => ({
